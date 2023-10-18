576b38286cba77349237baf9d8982e0919c32b1b
Tommy <zoom377@googlemail.com>
2023-10-18 16:15:50 +0100

    migrations are now applied automatically on web app start

--- DietTrackerBlazorServer/Program.cs
+++ DietTrackerBlazorServer/Program.cs
@@ -11,93 +11,116 @@ using DietTrackerBlazorServer.Model;
 using DietTrackerBlazorServer.Data;
 using DietTrackerBlazorServer.Areas.Identity;
 using DietTrackerBlazorServer.Services;
+using DietTrackerBlazorServer;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-var sqlVersion = new MariaDbServerVersion(new Version(1, 0));
-
-var dbContextBuilder = (DbContextOptionsBuilder options) =>
+internal class Program
 {
-    options.UseMySql(connectionString, sqlVersion);
-    options.LogTo(Console.WriteLine, LogLevel.Information);
-    if (builder.Environment.IsDevelopment())
+    static void Main(string[] args)
     {
-        //options.EnableSensitiveDataLogging();
-        //options.EnableDetailedErrors();
+        var builder = WebApplication.CreateBuilder(args);
+
+        AddDbContext(builder);
+
+        builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        {
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedAccount = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+
+        })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddBlazorise(options =>
+        {
+            options.Immediate = true;
+        })
+            .AddBootstrapProviders()
+            .AddFontAwesomeIcons();
+
+        builder.Services.AddTransient<ICorrelationCalculator, PearsonsRCorrelation>();
+
+
+        var app = builder.Build();
+
+        //init database
+        InitialiseDatabase(app);
+
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor
+            | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
+        });
+
+        //app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        //app.UseWebSockets();
+
+        app.MapControllers();
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+
+        app.Logger.LogInformation($"\"{app.Environment.ApplicationName}\" environment is \"{app.Environment.EnvironmentName}\"");
+
+        app.Run();
+
     }
-};
+    static void AddDbContext(WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        var sqlVersion = new MariaDbServerVersion(new Version(1, 0));
+        var dbContextBuilder = (DbContextOptionsBuilder options) =>
+        {
+            options.UseMySql(connectionString, sqlVersion);
+            options.LogTo(Console.WriteLine, LogLevel.Information);
+            if (builder.Environment.IsDevelopment())
+            {
+                //options.EnableSensitiveDataLogging();
+                //options.EnableDetailedErrors();
+            }
+        };
 
-builder.Services.AddDbContextFactory<ApplicationDbContext>(dbContextBuilder);
+        builder.Services.AddDbContextFactory<ApplicationDbContext>(dbContextBuilder);
+    }
 
-//builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
-//{
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    static void InitialiseDatabase(WebApplication app)
+    {
+        var dbcFactory = app.Services.GetRequiredService<IDbContextFactory<ApplicationDbContext>>();
+        using var scope = dbcFactory.CreateDbContext();
 
-//}, ServiceLifetime.Scoped);
+        scope.Database.Migrate();
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-{
-    options.SignIn.RequireConfirmedEmail = false;
-    options.SignIn.RequireConfirmedAccount = false;
-    options.SignIn.RequireConfirmedPhoneNumber = false;
+    //    using (var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+    //    {
+    //        scope.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.Migrate();
+    //        scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+    //        ...
+    //}
+    }
 
-})
-        .AddEntityFrameworkStores<ApplicationDbContext>()
-        .AddDefaultTokenProviders();
-
-
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddBlazorise(options =>
-{
-    options.Immediate = true;
-})
-    .AddBootstrapProviders()
-    .AddFontAwesomeIcons();
-
-builder.Services.AddTransient<ICorrelationCalculator, PearsonsRCorrelation>();
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
 }
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor
-    | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
-});
-
-//app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-//app.UseWebSockets();
-
-app.MapControllers();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-Console.WriteLine($"\"{app.Environment.ApplicationName}\" environment is \"{app.Environment.EnvironmentName}\"");
-
-app.Run();
